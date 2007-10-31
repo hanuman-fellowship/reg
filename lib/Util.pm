0b91260e12b870a6b4e7522a35eae6e8c80cda35
jbjornst <jbjornst>
2007-10-31 22:13:09 +0000

    Utility functions shared between Controllers will live here.

---
+++ lib/Util.pm
@@ -0,0 +1,61 @@
+use strict;
+use warnings;
+
+package Util;
+use base 'Exporter';
+our @EXPORT_OK = qw/affil_table/;
+
+use POSIX   qw/ceil/;
+
+my ($naffils, @affils, %checked);
+
+sub _affil_elem {
+    my ($i) = @_;
+    if ($i >= $naffils) {
+        return "<td>&nbsp;</td>";
+    }
+    my $a = $affils[$i];
+    my $id = $a->id();
+    my $descrip = $a->descrip();
+    return "<td><input type=checkbox name=aff$id "
+           . ($checked{$id} || "")
+           . ">"
+           . $descrip
+           . "</td>";
+}
+
+
+#
+# get the affiliations table ready for the template.
+# this was too hard to do within the template...
+# which affils should be checked?
+#
+# the first parameter is the Catalyst context.
+# the next are Afill objects that you want checked.
+#
+sub affil_table {
+    my ($c) = shift;
+    %checked = map { $_->id() => 'checked' } @_;
+
+    @affils = $c->model('RetreatCenterDB::Affil')->search(
+        undef,
+        { order_by => 'descrip' },
+    );
+    # figure the number of affils in the first and second column.
+    $naffils = @affils;
+    my $n = ceil($naffils/3);
+
+    my ($aff);
+    for my $i (0 .. $n-1) {
+        $aff .= "<tr>";
+
+        $aff .= _affil_elem($i);
+        $aff .= _affil_elem($i+$n);
+        $aff .= _affil_elem($i+2*$n);
+
+        $aff .= "</tr>\n";
+    }
+    $aff;
+}
+
+1;
