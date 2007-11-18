12467aad5af6b9905a808fc19035cbcdd1fd37e8
nshanker <nshanker>
2007-11-18 17:56:53 +0000

    Report Data object

---
+++ lib/RetreatCenterDB/Report.pm
@@ -0,0 +1,40 @@
+package RetreatCenterDB::Report;
+use base qw/DBIx::Class/;
+
+# Load required DBIC stuff
+__PACKAGE__->load_components(qw/PK::Auto Core/);
+# Set the table name
+__PACKAGE__->table('reports');
+# Set columns in table
+__PACKAGE__->add_columns(qw/
+    id 
+    descrip
+    format
+    zip_range
+    rep_order
+    last_run
+/);
+# Set the primary key for the table
+__PACKAGE__->set_primary_key(qw/id/);
+
+#
+# Set relationships:
+#
+
+# has_many():
+#   args:
+#     1) Name of relationship, DBIC will create accessor with this name
+#     2) Name of the model class referenced by this relationship
+#     3) Column name in *foreign* table
+__PACKAGE__->has_many(affil_report => 'RetreatCenterDB::AffilReport', 'report_id');
+
+# many_to_many():
+#   args:
+#     1) Name of relationship, DBIC will create accessor with this name
+#     2) Name of has_many() relationship this many_to_many() is shortcut for
+#     3) Name of belongs_to() relationship in model class of has_many() above
+#   You must already have the has_many() defined to use a many_to_many().
+__PACKAGE__->many_to_many(affils => 'affil_report', 'affil');
+
+
+1;
