ef9451999628977702e9d09e1c1ad27293eb4087
sahadev <sahadev>
2009-03-19 08:51:03 +0000

    init tinymce

---
+++ root/static/js/tiny_mce_config.js
@@ -0,0 +1,17 @@
+tinyMCE.init({
+    mode: "textareas",
+    theme: "advanced",
+    content_css: "/src/tiny_mce.css",
+    plugins: "tabfocus",
+    theme_advanced_resizing: true,
+    theme_advanced_resizing_horizontal: false,
+    theme_advanced_statusbar_location : "bottom",
+    theme_advanced_path : false,
+    theme_advanced_buttons1: "bold,italic,underline,fontsizeselect," 
+                            +"fontselect,bullist,numlist,"
+                            +"undo,redo,link,forecolor,backcolor",
+    theme_advanced_buttons2: "",
+    theme_advanced_buttons3: "",
+    theme_advanced_buttons4: "",
+    tabfocus_elements      : ":prev, :next",
+});
