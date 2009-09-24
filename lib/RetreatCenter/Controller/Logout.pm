de2e112aa26b02fa862508f10449734368a72d2c
sahadev <sahadev>
2009-09-24 03:13:04 +0000

    logout library to mmc home page

--- lib/RetreatCenter/Controller/Logout.pm
+++ lib/RetreatCenter/Controller/Logout.pm
@@ -6,11 +6,18 @@ use base 'Catalyst::Controller';
 sub index : Private {
     my ($self, $c) = @_;
 
+    my $username = $c->user->username();
+
     # Clear the user's state
     $c->logout;
 
-    # Send the user to the starting point
-    $c->response->redirect($c->uri_for('/login'));
+    if ($username eq 'library') {
+        $c->response->redirect("http://www.mountmadonna.org");
+    }
+    else {
+        # Send the user to the starting point
+        $c->response->redirect($c->uri_for('/login'));
+    }
 }
 
 1;
