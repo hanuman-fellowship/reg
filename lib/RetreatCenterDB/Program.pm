b8c11ec4c4d562c6b0c6e2f2f4a7ac9fdc426dae
sahadev <sahadev>
2010-04-21 23:20:22 +0000

    late notice edit hybrid program late notices should look to the parallel rental for the main meeting space.

--- lib/RetreatCenterDB/Program.pm
+++ lib/RetreatCenterDB/Program.pm
@@ -287,7 +287,8 @@ sub main_meeting_place {
                        $b->meeting_place->max() <=> $a->meeting_place->max()
                    }
                    grep { $_->breakout() ne 'yes' }
-                   $self->bookings();
+                   $self->rental_id()? $self->rental->bookings()
+                   :                   $self->bookings();
     @bookings? $bookings[0]->meeting_place()->name()
     :          "";
 }
