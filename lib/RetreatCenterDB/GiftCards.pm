439cbcf2c2a22c1ecd20d66e995d627f3cebdbbd
jonb108 <jonb@logicalpoetry.com>
2020-01-15 11:30:06 -0800

    second try on reg_id for GiftCards

--- lib/RetreatCenterDB/GiftCards.pm
+++ lib/RetreatCenterDB/GiftCards.pm
@@ -20,6 +20,7 @@ __PACKAGE__->add_columns(qw/
     the_date
     the_time
     transaction_id
+    reg_id
 /);
 
 # Set the primary key for the table
@@ -40,6 +41,7 @@ __PACKAGE__->belongs_to(person   => 'RetreatCenterDB::Person', 'person_id');
 1;
 __END__
 overview - Gift Cards - for lodging for PRs and other programs
+    either transaction_id or reg_id will be non-zero
 id - Primary key
 person_id - foreign key to Person
 code - the 5 character code of the card
@@ -47,6 +49,7 @@ amount - the dollar value of the card
 rec_fname - first name of the recipient
 rec_lname - last name of the recipient
 rec_email - email of the recipient
+reg_id - foreign key to Registration (optional)
 the_date - date purchase was made
 the_time - time purchase was made
-transaction_id - authorize.net transaction id for online payments
+transaction_id - authorize.net transaction id for online payments (optional)
