246f3c8f6b3a0be087ddf592337a526b6e40fdaf
jonb108 <jonb@logicalpoetry.com>
2022-04-08 18:09:57 -0700

    logged more in clear_old.pl

--- script/clear_old.pl
+++ script/clear_old.pl
@@ -5,5 +5,8 @@ use warnings;
 my @files = `ls -1t /var/Reg/backup`;
 chomp @files;
 my @to_delete = splice @files, 10;
+print scalar(localtime), "\n";
 print "in /var/Reg/backup deleting @to_delete\n";
-unlink @to_delete;
+for my $f (@to_delete) {
+    unlink $f or print "could not unlink $f: $!\n";
+}
