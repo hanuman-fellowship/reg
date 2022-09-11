a684673296f2e9f36e32804968da77b4bf51ae43
jonb108 <jonb@logicalpoetry.com>
2022-09-10 19:31:57 -0700

    inq sort status order

--- lib/RetreatCenter/Controller/Inquiry.pm
+++ lib/RetreatCenter/Controller/Inquiry.pm
@@ -54,13 +54,13 @@ sub list : Local {
     }
     elsif ($order eq 'status') {
         my %status_order = qw/
-            0 2 
-            1 1
-            2 6
-            3 4 
-            4 5
-            5 0
-            6 3
+            0 4   # new
+            1 1   # contacted
+            2 0   # engaged
+            3 2   # tentative
+            4 3   # rental
+            5 5   # denied by MMC
+            6 6   # denied by Host
         /;
         @inq = map {
                    $_->[1]
