bb500f009682ff10f3c00c933de7f3eb2aa16ee0
Jon Bjornstad <jon.bjornstad@gmail.com>
2023-01-11 18:34:22 -0800

    new test suites

---
+++ cgi-bin/omp.ts
@@ -0,0 +1,11 @@
+Test Suite for Online Member Payment
+
+https://akash.mountmadonna.org/cgi-bin/omp?code=
+
+and append these codes:
+
+dvrndz - Andrew Allen - General
+octjnt - Lyn Ackley - Sponsor
+
+ $50 - string mem_gen_amt
+$600 - string mem_spons_year
---
+++ cgi-bin/req_pay.ts
@@ -0,0 +1,10 @@
+Test Suite for Requested Payments
+
+Create a requested payment on a registration
+and Send it.  The requested payment could have
+several different items - which will be itemized.
+Click on the link in the email.
+
+Create a registration with a balance.
+Send the confirmation email.
+Click on the link in the email.
