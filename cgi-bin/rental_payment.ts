401b948e81cb3d92902862a1f78c9d3ff1088d54
Jon Bjornstad <jon.bjornstad@gmail.com>
2023-01-15 13:09:38 -0800

    rental_payment, update

---
+++ cgi-bin/rental_payment.ts
@@ -0,0 +1,15 @@
+Rental Payments
+
+can test by entering the URL directly
+
+parameters:
+code => from Web Grid link
+type => deposit or invoice
+
+invoice - asks for the balance
+
+Will need to adapt the controller code
+    and the email sent with contract and invoice
+    no need for rental_deposit and rental_invoice
+    no need for various strings - can remove
+    no need for send_...
