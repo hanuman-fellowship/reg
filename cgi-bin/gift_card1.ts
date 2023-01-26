4fa3f6c5af993e058132789db8c1a23b8d6352d0
Jon Bjornstad <jon.bjornstad@gmail.com>
2023-01-25 20:52:04 -0800

    tweak for gift card status link

--- cgi-bin/gift_card1.ts
+++ cgi-bin/gift_card1.ts
@@ -6,3 +6,28 @@ field check - missing field and valid email
     valid amount
 
 payment
+
+confirmation with transaction id
+
+emails - about the purchase with transaction_id
+       - to both with instructions and status link
+
+Configuration > Extra Accounts
+
+Configuration > Gift Cards
+    with a balance
+    or all
+
+use it when paying for registrations
+    payment narrative
+    payment
+        with balance left
+    after bringing the registration in
+        we see a negative charge - gift card
+    and a zero balance
+    reg history
+
+gift_card_status - nice
+
+add more $
+    and use again
