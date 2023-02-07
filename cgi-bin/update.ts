310368243b66753887391a59c3e80e034174b91d
Jon Bjornstad <jon.bjornstad@gmail.com>
2023-02-07 14:38:49 -0800

    update.ts and DDUP on person screen

---
+++ cgi-bin/update.ts
@@ -0,0 +1,12 @@
+Individuals Updating their address, phone, etc
+
+On a person's screen click on the DDUP link.
+DDUP stands for Distributed Data UpDate.
+We can send people a 'secure' link so they can
+keep their demographic information up to date themselves.
+
+This has never been used.
+
+Need to do something about hackers guessing secure codes...
+Perhaps just alert the authorities and shut it down if
+more than one incorrect code is entered in a few minutes time?
