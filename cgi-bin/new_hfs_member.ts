142fd3d0bc7fdec232fcaaa0931147a4129d94bd
Jon Bjornstad <jon.bjornstad@gmail.com>
2023-02-07 13:22:19 -0800

    get talks, new hfs test suite

---
+++ cgi-bin/new_hfs_member.ts
@@ -0,0 +1,11 @@
+Test Suite for new_hfs_member
+
+existing person or not
+
+3 levels of membership
+
+TODO: create the member
+    from grab_new
+
+TODO: omp - add the payment automatically
+    from grab_new
