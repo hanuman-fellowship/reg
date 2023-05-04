853c32d0b1e996552eaceee596e4e94f966c7213
Jon Bjornstad <jon.bjornstad@gmail.com>
2023-05-04 10:41:15 -0700

    styling help, ensure fancy styling only on akash2

--- lib/Util.pm
+++ lib/Util.pm
@@ -2784,7 +2784,9 @@ sub fee_types {
 
 sub styled {
     my ($fname) = @_;
-    return -f '/var/Reg/documents/plain_style'? $fname: "new_tt2/$fname";
+    return   -f '/var/Reg/documents/plain_style'? $fname 
+           : -f '/var/www/src/cgi-bin/akash2'   ? "new_tt2/$fname"
+           :                                      $fname;
 }
 
 1;
